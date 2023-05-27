4500ce3f17382d34be7242c2e54dc55d1ee3bada
Aetus Silicon <aetussilicon@proton.me>
2023-05-26 21:11:40 -0300

    Projeto spring initializer

---
+++ src/test/java/com/budget/budgetAPI/BudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.budgetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
